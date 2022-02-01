b970cc15c6db79583ce29e199f8972ec07c943cd
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-02-01 19:03:50 +0300

    Add files via upload

---
+++ Date Exercise With data base/Date/SQLQuery2.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO [dbo].[test] ([id], [name], [family],[lastname],[number], [passport],[adress],[email]  )  
+VALUES (1, 'Иван', 'Примский','Владимирович',89435678789,0111456798,'st putersburg','a@mail.ru');
