c02125bc64ef7e745064401ee9b531565a52354d
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-02-01 17:51:34 +0300

    Add files via upload

---
+++ Date/Date/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Date
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testDataSet1.test". При необходимости она может быть перемещена или удалена.
+            this.testTableAdapter.Fill(this.testDataSet1.test);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "testDataSet.main". При необходимости она может быть перемещена или удалена.
+            this.mainTableAdapter.Fill(this.testDataSet.main);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            AddForm af = new AddForm();
+            af.Owner = this;
+            af.Show();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Удалить запись?", "Удаление", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
